8e126abe677a9d3130b5875db14244a7d8540a0c
AkashKank <akashkank4@gmail.com>
2022-03-19 00:00:02 +0530

    Reference

---
+++ Reference/01 Reference Variable Refering To Normal Variable.cpp
@@ -0,0 +1,31 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int No=0;
+   int &Ref=No;
+
+   cout<<"Value Of No is = "<<No<<endl;
+   cout<<"\nValue Of Reference Variable(Ref) is = "<<Ref<<endl;
+
+   cout<<"\n***********************************************\n";
+
+   Ref=45;     // initialisation Of Reference Variable
+
+   cout<<"\nValue Of No is = "<<No<<endl;
+   cout<<"\nValue Of Reference Variable(Ref) is = "<<Ref<<endl;
+
+   cout<<"\n***********************************************\n";
+
+   No++;     // Increment In Normal Variable
+
+   cout<<"\nValue Of No is = "<<No<<endl;
+   cout<<"\nValue Of Reference Variable(Ref) is = "<<Ref<<endl;
+
+   cout<<"\n***********************************************\n";
+
+   getch();
+   return 0;
+}
